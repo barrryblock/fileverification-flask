9902adc4deeccab274757049743c9fc761c1d808
John Prasad <blockmanbarry10@hotmail.com>
2024-07-17 21:28:54 +0100

    Changes to adopt render_template

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from azure.cosmos import CosmosClient, exceptions
 from pymongo import MongoClient, errors
 from azure.storage.blob import BlobServiceClient
-from flask import Flask, abort, jsonify, request, redirect
+from flask import Flask, abort, jsonify, request, redirect, flash, render_template
 import os
 
 
@@ -77,45 +77,9 @@ def attest_device():
     else:
         abort(403, 'Invalid device token or device not registered.')
 
-@app.route("/")
-def view_files():
-    blob_items = container_client.list_blobs() # list all the blobs in the container
-
-    img_html = "<div style='display: flex; justify-content: space-between; flex-wrap: wrap;'>"
-
-    for blob in blob_items:
-        blob_client = container_client.get_blob_client(blob=blob.name) # get blob client to interact with the blob and get blob url
-        img_html += "<img src='{}' width='auto' height='200' style='margin: 0.5em 0;'/>".format(blob_client.url) # get the blob url and append it to the html
-    
-    img_html += "</div>"
-
-    # return the html with the images
-    return """
-    <head>
-    <!-- CSS only -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    </head>
-    <body>
-        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-            <div class="container">
-                <a class="navbar-brand" href="/">files App</a>
-            </div>
-        </nav>
-        <div class="container">
-            <div class="card" style="margin: 1em 0; padding: 1em 0 0 0; align-items: center;">
-                <h3>Upload new File</h3>
-                <div class="form-group">
-                    <form method="post" action="/upload-files" 
-                        enctype="multipart/form-data">
-                        <div style="display: flex;">
-                            <input type="file" accept=".png, .jpeg, .jpg, .gif" name="files" multiple class="form-control" style="margin-right: 1em;">
-                            <input type="submit" class="btn btn-primary">
-                        </div>
-                    </form>
-                </div> 
-            </div>
-        
-    """ + img_html + "</div></body>"
+@app.route('/')
+def index():
+    return render_template("index.html")
 
 @app.route("/api/files", methods=["GET"])
 def get_files_json():
